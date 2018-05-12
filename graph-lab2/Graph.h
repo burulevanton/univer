8b71cf9e79cfa6b83a68746e884dc41423d06649
burulevanton <burulyov.anton@yandex.ru>
2018-05-12 11:24:57 +0500

    delete files

--- graph-lab2/Graph.h
+++ graph-lab2/Graph.h
@@ -12,7 +12,43 @@
 #include <limits>
 #include <queue>
 
-#include "DSU.h"
+
+class DSU {
+private:
+    std::vector<int> p;
+    std::vector<int> rank;
+public:
+    explicit DSU(int n);
+    void makeSet(int x);
+    int find(int x);
+    void unite(int x, int y);
+};
+
+DSU::DSU(int n) {
+    this->p = std::vector<int>(static_cast<size_t>(n));
+    this->rank = std::vector<int>(static_cast<size_t>(n));
+}
+
+void DSU::makeSet(int x) {
+    p[x] = x;
+    rank[x] = 0;
+}
+
+int DSU::find(int x) {
+    return (x == p[x] ? x : p[x] = find(p[x]));
+}
+
+void DSU::unite(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if (rank[x] < rank[y])
+        p[x] = y;
+    else{
+        p[y] = x;
+        if (rank[x] == rank[y])
+            ++rank[x];
+    }
+}
 
 class GraphAdjMatrix;
 class GraphAdjList;
